aca1db599504fb741e43108dac00d3f52c03ae06
eung-eung <boyvip5231@gmail.com>
2024-06-11 01:02:49 +0700

    handle middleware routes

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server'
+
+
+export async function middleware(request: NextRequest) {
+    const secret = process.env.NEXTAUTH_SECRET
+    const token = await getToken({ req: request, secret });
+
+    if (!token) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+}
+export const config = {
+    matcher: [
+        '/',
+        '/contracts',
+        '/contracts/payments',
+        '/cars',
+        '/chat',
+        '/accounts',
+        '/ratings',
+        '/payments'
+    ]
+}
