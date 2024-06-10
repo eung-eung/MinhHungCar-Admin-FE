812063ea2025b8ef74c87c04681076f4bd719685
eung-eung <boyvip5231@gmail.com>
2024-06-11 01:12:55 +0700

    layout for login page

---
+++ src/app/(views)/login/layout.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Đăng nhập'
+}
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return children
+}
