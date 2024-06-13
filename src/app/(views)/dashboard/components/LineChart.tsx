559b71e2ba69c2c7be7500adeac5f35472745408
eung-eung <boyvip5231@gmail.com>
2024-06-14 01:16:16 +0700

    build LineChart component

---
+++ src/app/(views)/dashboard/components/LineChart.tsx
@@ -0,0 +1,84 @@
+'use client'
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area } from 'recharts';
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
+export default function LineChartRef() {
+
+    return (
+        <ResponsiveContainer>
+            <AreaChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 30,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                        <stop offset="100%" stopColor="#8884d8" stopOpacity={0} />
+                    </linearGradient>
+                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#e788dc" stopOpacity={0.8} />
+                        <stop offset="100%" stopColor="#e788dc" stopOpacity={0} />
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Area dot type="linear" dataKey="pv" fillOpacity={1} fill='url(#colorUv)' stroke="#62c3b6" />
+                <Area dot type="linear" dataKey="uv" fillOpacity={1} fill='url(#colorPv)' stroke="#b919a6" />
+            </AreaChart>
+        </ResponsiveContainer>
+    );
+}
