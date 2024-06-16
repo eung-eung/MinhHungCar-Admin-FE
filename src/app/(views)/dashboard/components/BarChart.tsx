4958f90d0dbcb537774bf81dacd4e74ea8c8c150
eung-eung <boyvip5231@gmail.com>
2024-06-16 19:38:17 +0700

    update component

--- src/app/(views)/dashboard/components/BarChart.tsx
+++ src/app/(views)/dashboard/components/BarChart.tsx
@@ -48,25 +48,26 @@ const data = [
 ];
 export default function BarRefChart() {
     return (
-        <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-            }}
-        >
-            {/* <CartesianGrid strokeDasharray="3 3" /> */}
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
-            <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
-
+        <>
+            <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                {/* <CartesianGrid strokeDasharray="3 3" /> */}
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
+                <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+        </>
     );
 }
