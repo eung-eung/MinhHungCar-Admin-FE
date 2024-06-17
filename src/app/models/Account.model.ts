91e1bde89d597244af8ed5a9efd2a2dd6752a752
eung-eung <boyvip5231@gmail.com>
2024-06-17 15:38:59 +0700

    build account interface

--- src/app/models/Account.model.ts
+++ src/app/models/Account.model.ts
@@ -1,3 +1,19 @@
-export interface Account {
+import { Role } from "./Role"
 
+export interface IAccount {
+    id: number;
+    roleid: number;
+    role: Role;
+    first_name: string;
+    last_name: string;
+    phone_number: string;
+    email: string;
+    identification_card_number: string;
+    password: string;
+    avatar_url: string;
+    driving_license: string;
+    status: string;
+    date_of_birth: string;
+    created_at: Date;
+    updated_at: Date;
 }
