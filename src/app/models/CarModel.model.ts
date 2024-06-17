bf1ac7779697fafce9f26842117ecf5aafe8d0f5
eung-eung <boyvip5231@gmail.com>
2024-06-17 15:39:43 +0700

    build car model interface

---
+++ src/app/models/CarModel.model.ts
@@ -0,0 +1,10 @@
+export interface ICarModel {
+    id: number;
+    brand: string;
+    model: string;
+    year: number;
+    number_of_seats: number;
+    based_price: number;
+    created_at: Date;
+    updated_at: Date;
+}
