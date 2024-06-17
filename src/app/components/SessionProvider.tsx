ce9ee889a77437e7ce4b51a34e19ce3b12a365b8
eung-eung <boyvip5231@gmail.com>
2024-06-17 15:39:32 +0700

    build handle session status provider

---
+++ src/app/components/SessionProvider.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { ConfigProvider, Spin } from "antd"
+import { useSession } from "next-auth/react"
+
+export default function SessionProvider({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const { status } = useSession()
+
+    return (
+        <>
+            {status === 'loading' ? <Spin size="large" fullscreen /> : children}
+        </>
+    )
+}
