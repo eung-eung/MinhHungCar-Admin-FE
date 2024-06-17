4aa9de87b7139baa0f58dc3c31198e85e8940f8e
eung-eung <boyvip5231@gmail.com>
2024-06-17 15:39:12 +0700

    build car interface

---
+++ src/app/models/Car.model.ts
@@ -0,0 +1,20 @@
+import { IAccount } from "./Account.model"
+import { ICarModel } from "./CarModel.model"
+
+export interface ICar {
+    id: number;
+    partner_id: number;
+    account: IAccount;
+    car_model_id: number;
+    car_model: ICarModel;
+    license_plate: string;
+    parking_lot: string;
+    description: string;
+    fuel: string;
+    motion: string;
+    price: number;
+    status: string;
+    period: number;
+    created_at: Date;
+    updated_at: Date;
+}
