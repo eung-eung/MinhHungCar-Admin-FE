10e95fa49b267b8ef3dd344de0d514ab44dce14b
eung-eung <boyvip5231@gmail.com>
2024-06-17 15:43:05 +0700

    change to netx-public .env

--- src/app/utils/axios.ts
+++ src/app/utils/axios.ts
@@ -1,11 +1,10 @@
 import axios from "axios"
-console.log(process.env.API_ENDPOINT);
 
 export default axios.create({
-    baseURL: process.env.API_ENDPOINT,
+    baseURL: process.env.API_ENDPOINT_PUBLIC,
     headers: { "Content-Type": "application/json" }
 })
 
 export const axiosAuth = axios.create({
-    baseURL: process.env.API_ENDPOINT
+    baseURL: process.env.API_ENDPOINT_PUBLIC
 })
