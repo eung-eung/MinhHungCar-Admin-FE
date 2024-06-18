74134a7d5118bff419828ebec0c3d5e55cb260c0
eung-eung <boyvip5231@gmail.com>
2024-06-18 14:24:10 +0700

    build I18next Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "./layouts/Layout";
 import { Metadata } from "next";
-
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import SessionProvider from "./components/SessionProvider";
 import AuthProvider from "./components/AuthProvider";
+import I18nProvider from "./components/I18Provider";
+
+
+
 
 
 export const metadata: Metadata = {
@@ -15,16 +18,25 @@ export const metadata: Metadata = {
 
 const inter = Inter({ subsets: ["latin"] });
 
+export default function RootLayout(
+  {
+    children, }: Readonly<
+      {
+        children: React.ReactNode,
+      }
+    >) {
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+
   return (
-    <html lang="en">
+    <html>
       <body className={inter.className}>
         <AntdRegistry>
           <AuthProvider>
             <SessionProvider>
               <Layout>
-                {children}
+                <I18nProvider>
+                  {children}
+                </I18nProvider>
               </Layout>
             </SessionProvider>
           </AuthProvider>
