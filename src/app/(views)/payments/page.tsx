045b0c68481bd782e001fc6f832fb448b8209a2d
eung-eung <boyvip5231@gmail.com>
2024-06-19 22:35:19 +0700

    build skeleton page

--- src/app/(views)/payments/page.tsx
+++ src/app/(views)/payments/page.tsx
@@ -1,7 +1,33 @@
+'use client'
+import TopFilterTable from '@/app/components/TopFilterTable'
 import React from 'react'
+import PaymentTable from './components/PaymentTable'
 
 export default function Payments() {
+
+    const handleChange = () => {
+
+    }
+
     return (
-        <div>Payments</div>
+        <>
+            <TopFilterTable
+                handleChange={handleChange}
+                optionList={[
+                    {
+                        value: 'waiting_payment',
+                        label: 'Chờ thanh toán'
+                    },
+                    {
+                        value: "complete",
+                        label: 'Đã thanh toán'
+                    }
+                ]}
+                defaultValue='waiting_payment'
+                showSearch={false}
+                showDatepicker={true}
+            />
+            <PaymentTable />
+        </>
     )
 }
