d6dce1044612c1006327360e2a0073ab4aa77f8f
eung-eung <boyvip5231@gmail.com>
2024-06-19 22:36:59 +0700

    build slug route for contract

---
+++ src/app/(views)/cars/contract/[carIdSlug]/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import React, { ReactElement, useEffect, useState } from 'react'
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import type { ToolbarProps, ToolbarSlot, TransformToolbarSlot } from '@react-pdf-viewer/toolbar';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+export default function ContractPage({
+    params: { carIdSlug }
+}: {
+    params: { carIdSlug: any }
+}) {
+    const [pdfUrl, setPdfUrl] = useState<any>()
+    const transform: TransformToolbarSlot = (slot: ToolbarSlot) => ({
+        ...slot,
+        Open: () => <></>,
+        DownloadMenuItem: () => <></>,
+        EnterFullScreen: () => <></>,
+        EnterFullScreenMenuItem: () => <></>,
+        SwitchTheme: () => <></>,
+        SwitchThemeMenuItem: () => <></>,
+        GoToNextPage: () => <></>,
+        NumberOfPages: () => <></>,
+        CurrentPageInput: () => <></>
+    });
+
+    const renderToolbar = (Toolbar: (props: ToolbarProps) => React.ReactElement) => (
+        <Toolbar>{renderDefaultToolbar(transform)}</Toolbar>
+    );
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+        renderToolbar,
+    });
+    const { renderDefaultToolbar } = defaultLayoutPluginInstance.toolbarPluginInstance;
+
+    useEffect(() => {
+        setPdfUrl('https://rentalcar-capstone.s3.ap-southeast-2.amazonaws.com/df2827bd-961a-4b70-96fe-059e1d4dc0fd.pdf')
+    }, [carIdSlug])
+    return (
+
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <Viewer
+                fileUrl={pdfUrl}
+                plugins={[defaultLayoutPluginInstance]}
+            />
+        </Worker>
+
+
+    )
+}
