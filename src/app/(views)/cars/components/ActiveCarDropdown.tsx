018b6485b5b9d16386a65e27a7babb6727afb4a4
eung-eung <boyvip5231@gmail.com>
2024-06-19 22:27:53 +0700

    build active car dropdown component

---
+++ src/app/(views)/cars/components/ActiveCarDropdown.tsx
@@ -0,0 +1,51 @@
+import { Button, Dropdown, Menu } from 'antd'
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import { useRouter } from 'next/router';
+export default function ActiveCarDropdown(
+    {
+        id,
+        handleOpenDetailDialog,
+    }: {
+        id: any,
+        handleOpenDetailDialog: (id: any) => void,
+    }
+) {
+    const showCarContract = async (id: any) => {
+        window.open('/cars/contract/' + id)
+    }
+    const cancelCarContract = (id: any) => {
+
+    }
+    return (
+        <Dropdown
+            dropdownRender={() => (
+                <Menu
+                    items={[
+                        {
+                            key: '1',
+                            label: 'Chi tiết',
+                            onClick: () =>
+                                handleOpenDetailDialog(id)
+                        },
+                        {
+                            key: '2',
+                            label: 'Hợp đồng',
+                            onClick: () =>
+                                showCarContract(id)
+                        },
+                        {
+                            key: '3',
+                            label: 'Hủy hợp đồng',
+                            onClick: () =>
+                                cancelCarContract(id)
+                        },
+                    ]}>
+
+                </Menu>
+            )}
+
+            placement="bottom" arrow>
+            <Button><MoreHorizOutlinedIcon /></Button>
+        </Dropdown>
+    )
+}
