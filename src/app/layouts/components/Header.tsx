dc13198555c0f023f746b26b6d7c8500ac0d22ab
eung-eung <boyvip5231@gmail.com>
2024-06-22 14:09:11 +0700

    import digital button component

--- src/app/layouts/components/Header.tsx
+++ src/app/layouts/components/Header.tsx
@@ -3,15 +3,19 @@ import React from 'react'
 import classes from './Header.module.css'
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import DigitalClock from '@/app/components/DigitalClock';
 export default function Header() {
     return (
         <div className={classes.container + ' flex justify-end items-center'}>
+            <DigitalClock />
+            {/* <div className='flex justify-end items-center'> */}
             <div className={classes.notification}>
-                <NotificationsNoneIcon sx={{ color: '#fff', fontSize: '25px' }} />
+                <NotificationsNoneIcon sx={{ color: '#000000', fontSize: '25px' }} />
             </div>
             <Avatar size='default' src='/Minhhung.png' />
             <p className={classes.text}>Admin</p>
             <KeyboardArrowDownOutlinedIcon sx={{ color: '#000000', fontSize: '20px' }} />
+            {/* </div> */}
         </div>
     )
 }
