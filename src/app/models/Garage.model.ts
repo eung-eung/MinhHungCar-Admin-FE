9a930edf974d23927746f42d00c32a542668c049
eung-eung <boyvip5231@gmail.com>
2024-06-25 00:14:46 +0700

    update type

--- src/app/models/Garage.model.ts
+++ src/app/models/Garage.model.ts
@@ -1,8 +1,8 @@
 export interface IGarage {
-    max_7_seats: number;
-    max_4_seats: number;
-    max_15_seats: number;
-    total: number;
+    max_7_seats?: any;
+    max_4_seats?: any;
+    max_15_seats?: any;
+    total?: any;
 }
 
 export interface IGarageRequest {
