0cad022cb47ed69d4ce224840eb012e034fe312c
eung-eung <boyvip5231@gmail.com>
2024-06-25 00:14:24 +0700

    show error when input invalid

--- src/app/(views)/cars/components/GarageConfigDialog.tsx
+++ src/app/(views)/cars/components/GarageConfigDialog.tsx
@@ -1,6 +1,6 @@
 import { IGarage, IGarageRequest } from '@/app/models/Garage.model'
 import useAxiosAuth from '@/app/utils/hooks/useAxiosAuth'
-import { Button, Modal, Tag } from 'antd'
+import { Button, Flex, Modal, Tag } from 'antd'
 import { ChangeEvent, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Bounce, ToastContainer, toast } from 'react-toastify'
@@ -23,6 +23,11 @@ export default function GarageConfigDialog(
     const [garageConfig, setGarageConfig] = useState<IGarage>()
     const [loading, setLoading] = useState<boolean>(true)
     const { t } = useTranslation()
+    const [error, setError] = useState<any>({
+        max_4_seats: false,
+        max_7_seats: false,
+        max_15_seats: false
+    })
     const keysToRemove = ["current_4_seats", "current_7_seats", "current_15_seats", "current_total"];
     const getGarageConfig = async () => {
         setLoading(true)
@@ -40,16 +45,16 @@ export default function GarageConfigDialog(
     const handleOk = async () => {
         setConfirmLoading(true);
         const config = {} as IGarageRequest
-        config.max_7_seats = garageConfig?.max_7_seats
-        config.max_4_seats = garageConfig?.max_4_seats
-        config.max_15_seats = garageConfig?.max_15_seats
+        config.max_7_seats = parseInt(garageConfig?.max_7_seats)
+        config.max_4_seats = parseInt(garageConfig?.max_4_seats)
+        config.max_15_seats = parseInt(garageConfig?.max_15_seats)
 
         try {
             const response = await axiosAuth.put('/admin/garage_config', config)
             if (response.status === 200) {
                 toast.success('Cập nhật thành công', {
-                    position: "top-right",
-                    autoClose: 5000,
+                    position: "top-center",
+                    autoClose: 2000,
                     hideProgressBar: false,
                     closeOnClick: true,
                     pauseOnHover: false,
@@ -84,23 +89,32 @@ export default function GarageConfigDialog(
     const handleCancel = () => {
         setOpen(false);
         setConfirmLoading(false);
+        setError((prev: any) => ({
+            ...prev,
+            max_4_seats: false,
+            max_7_seats: false,
+            max_15_seats: false
+        }))
     };
 
     const handleOnChange = (e: ChangeEvent<HTMLInputElement>, key: any) => {
-        const numberRegex = /^[1-9][0-9]*$/;
-
-        if (!numberRegex.test(e.target.value) && e.target.value !== "") {
-            return
+        const numberRegex = /^\d+$/;
+        if (!numberRegex.test(e.target.value)) {
+            setError((prev: any) => ({
+                ...prev, [key]: true
+            }))
+        } else {
+            setError((prev: any) => ({
+                ...prev, [key]: false
+            }))
         }
         setGarageConfig((config: any) => ({
             ...config,
-            [key]: e.target.value === "" ? 1 : parseInt(e.target.value)
+            [key]: (e.target.value)
         }))
     }
-
     return (
         <>
-
             <Modal
                 title={
                     <Tag
@@ -127,9 +141,24 @@ export default function GarageConfigDialog(
                     {!loading &&
                         Object?.keys(garageConfig as IGarage).map((key) => {
                             if (key === 'total') return
-                            return <div className='flex items-center mb-4'>
+                            return <div className='mb-5'>
                                 <p className='flex-1/2 w-1/5'>{t(`common:${key}`)}</p>
-                                <Input type='text' value={(garageConfig as IGarage)[key as KeyIGarage]} onChange={(e) => handleOnChange(e, key)} />
+                                <div className='flex flex-col'>
+                                    <Input
+                                        type='text'
+                                        value={(garageConfig as IGarage)[key as KeyIGarage]}
+                                        onChange={(e) => handleOnChange(e, key)}
+                                    />
+                                    {
+                                        error[key]
+                                            ? <Tag color='red'>
+                                                {t(`common:${key}`) + ' phải là số'}
+                                            </Tag>
+                                            :
+                                            ''
+                                    }
+                                </div>
+
                             </div>
                         })
 
