37ed8f17db4e000143fec2c27115088fc1756f6b
eung-eung <boyvip5231@gmail.com>
2024-06-24 15:37:36 +0700

    show detail account

--- src/app/(views)/accounts/components/AccountDialog.tsx
+++ src/app/(views)/accounts/components/AccountDialog.tsx
@@ -1,78 +1,88 @@
 import React from 'react'
-import { Image } from 'antd'
+import { Button, Image } from 'antd'
 import classes from './index.module.css'
 import { Tag } from 'antd'
-export default function AccountDialog() {
+import { IAccount } from '@/app/models/Account.model'
+import { useTranslation } from 'react-i18next'
+export default function AccountDialog({ detail }: { detail?: IAccount }) {
+    console.log(detail);
+    const { t } = useTranslation()
     return (
         <div className={classes.diaglog}>
             <div className={classes.diaglogBody}>
                 <div className={classes.topBody}>
-                    <Image src='/avatarSample.jpg'
-                        width={100}
-                        height={100}
-                        style={{ objectFit: "contain" }}
-                        alt='Ảnh tài khoản'
-                    />
-                    <div className={classes.accountInfor}>
-                        <p>Nguyễn Văn Long</p>
-                        {/* item */}
-                        <div className={classes.inforItem}>
-                            <p className={classes.label}>
-                                CCCD
-                            </p>
-                            <p className={classes.infor}>
-                                00000000
-                            </p>
-                        </div>
-                        {/* item */}
-                        <div className={classes.inforItem}>
-                            <p className={classes.label}>
-                                Vai trò
-                            </p>
-                            <p className={classes.infor}>
-                                <Tag color='#4baf21'>
-                                    Khách Hàng
-                                </Tag>
-                            </p>
-                        </div>
-                        {/* item */}
-                        <div className={classes.inforItem}>
-                            <p className={classes.label}>
-                                Email
-                            </p>
-                            <p className={classes.infor}>
-                                boyvip5231@gmail.com
-                            </p>
-                        </div>
-                        {/* item */}
-                        <div className={classes.inforItem}>
-                            <p className={classes.label}>
-                                Số điện thoại
-                            </p>
-                            <p className={classes.infor}>
-                                00000000
-                            </p>
-                        </div>
+
+                    <div className={classes.inforItem}>
+                        <p className={classes.label}>
+                            Tên
+                        </p>
+                        <p className={classes.infor}>
+                            {detail?.first_name + ' ' + detail?.last_name}                            </p>
                     </div>
+                    {/* item */}
+                    <div className={classes.inforItem}>
+                        <p className={classes.label}>
+                            CCCD
+                        </p>
+                        <p className={classes.infor}>
+                            {detail?.identification_card_number}
+                        </p>
+                    </div>
+                    {/* item */}
+                    <div className={classes.inforItem}>
+                        <p className={classes.label}>
+                            Vai trò
+                        </p>
+                        <p className={classes.infor}>
+                            <Tag
+                                color={detail?.role === 'customer' ? 'cyan' : 'blue'}>
+                                {t(`common:${detail?.role}`)}
+                            </Tag>
+
+
+                        </p>
+                    </div>
+                    {/* item */}
+                    <div className={classes.inforItem}>
+                        <p className={classes.label}>
+                            Email
+                        </p>
+                        <p className={classes.infor}>
+                            {detail?.email}
+                        </p>
+                    </div>
+                    {/* item */}
+                    <div className={classes.inforItem}>
+                        <p className={classes.label}>
+                            Số điện thoại
+                        </p>
+                        <p className={classes.infor}>
+                            {detail?.phone_number}
+                        </p>
+                    </div>
+                </div>
+                <div className='mt-3 w-full text-right'>
+                    <Tag style={{ fontWeight: 600 }} color={detail?.status === 'active' ? "green" : "red"}>
+                        {t(`accountStatus:${detail?.status}`)}
+                    </Tag>
                 </div>
                 {/* bottom body */}
-                <div className={classes.bodyBottom}>
-                    <p className={classes.label}>
-                        Giấy phép lái xe
-                    </p>
-                    <div className={classes.licenseImages}>
-                        <Image
-                            width={200}
-                            src="https://cdn.thuvienphapluat.vn/tintuc/uploads/image/2021/01/27/can-cuoc-cong-dan-gan-chip-2(1).jpg"
-                        />
-                        <Image
-                            width={200}
-                            src="https://cdn.thuvienphapluat.vn/tintuc/uploads/image/2021/01/27/can-cuoc-cong-dan-gan-chip-2(1).jpg"
-                        />
+                {
+                    detail?.driving_license_images != null &&
+                    <div className={classes.bodyBottom}>
+                        <p className={classes.label}>
+                            Giấy phép lái xe
+                        </p>
+                        <div className={classes.licenseImages}>
+                            {detail?.driving_license_images.map(img =>
+                                <Image
+                                    width={200}
+                                    src={img} />
+                            )}
+                        </div>
                     </div>
-
-                </div>
+                }
             </div>
-        </div>
+        </div >
     )
 }
