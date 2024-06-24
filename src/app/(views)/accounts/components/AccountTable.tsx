6d392b4e4480d49a0ffc10f614daa970068c30f1
eung-eung <boyvip5231@gmail.com>
2024-06-24 15:57:23 +0700

    update width dialog

--- src/app/(views)/accounts/components/AccountTable.tsx
+++ src/app/(views)/accounts/components/AccountTable.tsx
@@ -99,7 +99,7 @@ export default function AccountTable(
                 dataSource={accountData}
                 columns={columns} />
             <Diaglog
-                width='40%'
+                width='45%'
                 loading={loadingDialog}
                 setOpen={setOpen}
                 title="Chi tiết tài khoản"
