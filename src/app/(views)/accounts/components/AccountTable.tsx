5eb22b90d91fe81874418e6569b62f468ee1635b
eung-eung <boyvip5231@gmail.com>
2024-06-25 00:11:43 +0700

    handle update status account

--- src/app/(views)/accounts/components/AccountTable.tsx
+++ src/app/(views)/accounts/components/AccountTable.tsx
@@ -1,13 +1,18 @@
 'use client'
 import { IAccount } from '@/app/models/Account.model'
-import { Table, Tag } from 'antd'
+import { Modal, Table, Tag } from 'antd'
 import React, { useState } from 'react'
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import { useTranslation } from 'react-i18next';
 import AccountDialog from './AccountDialog';
 import Diaglog from '@/app/components/Modal';
 import useAxiosAuth from '@/app/utils/hooks/useAxiosAuth';
+type IUpdateAccountStatusRequest = {
 
+    account_id: any,
+    status: any
+
+}
 export default function AccountTable(
     {
         accountData,
@@ -26,6 +31,34 @@ export default function AccountTable(
     const axiosAuth = useAxiosAuth()
     const [accountDetail, setAccountDetail] = useState<IAccount>()
     const [loadingDialog, setLoadingDialog] = useState<boolean>(true)
+
+    const handleUpdateStatusAccount = async (id: any, status: any, title: any) => {
+        showConfirmModal(title)
+            .then(async () => {
+                const response = await axiosAuth.put('/admin/account/status', {
+                    account_id: id,
+                    status: status
+                } as IUpdateAccountStatusRequest)
+                setRefresh(prev => !prev)
+            })
+    }
+
+    const showConfirmModal = (title: any) => {
+        const { confirm } = Modal
+        return new Promise((res, rej) => {
+            confirm({
+                title: title,
+                onOk: () => {
+                    res(true)
+                },
+                cancelText: "Hủy",
+                onCancel: () => {
+                    rej(true)
+                }
+            })
+        })
+    }
+
     const handleOpenDetailDialog = async (id: any) => {
         setOpen(true)
         setLoadingDialog(true)
@@ -83,10 +116,31 @@ export default function AccountTable(
                     <RemoveRedEyeOutlinedIcon
                         onClick={() => handleOpenDetailDialog(record.id)}
                         className='mr-3 cursor-pointer' />
+                    {
+                        record.status === 'active' ? <Tag onClick={
+                            () =>
+                                handleUpdateStatusAccount(
+                                    record.id,
+                                    'inactive',
+                                    'Bạn có muốn khóa tài khoản này?'
+                                )}
+                            color='red'
+                            className='ml-3 cursor-pointer'>
+                            Khóa
+                        </Tag> :
+                            <Tag onClick={
+                                () =>
+                                    handleUpdateStatusAccount(
+                                        record.id,
+                                        'active',
+                                        'Bạn có muốn gỡ khóa tài khoản này?'
+                                    )}
+                                color='default'
+                                className='ml-3 cursor-pointer'>
+                                Gỡ khóa
+                            </Tag>
+                    }
 
-                    <Tag color='#ef1a2b' className='ml-3 cursor-pointer'>
-                        Khóa
-                    </Tag>
                 </div>
 
             )
