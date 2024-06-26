f45d086642766dd715dfc24d112e623497a5688b
eung-eung <boyvip5231@gmail.com>
2024-06-26 15:22:30 +0700

    build toast util

---
+++ src/app/utils/toast.ts
@@ -0,0 +1,26 @@
+import toast from "react-hot-toast";
+
+export const sucessNotify = (title: any) => toast.success(title, {
+    style: {
+        padding: '5px',
+        color: '#67C23A',
+        background: '#F0F9EB'
+    },
+    iconTheme: {
+        primary: 'green',
+        secondary: 'white',
+    },
+})
+
+export const errorNotify = (title: any) => toast.error(title, {
+    style: {
+        padding: '5px',
+        color: '#F56C6C',
+        background: '#FFEFF0',
+        fontSize: 14
+    },
+    iconTheme: {
+        primary: 'red',
+        secondary: 'white',
+    },
+});
