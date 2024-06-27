ba398b2405edee4ea1e53cc1a25167daa0f7aaf6
eung-eung <boyvip5231@gmail.com>
2024-06-27 21:26:15 +0700

    customer contract model

---
+++ src/app/models/CustomerContract.ts
@@ -0,0 +1,23 @@
+import { IAccount } from "./Account.model"
+import { ICar } from "./Car.model"
+
+export interface ICustomerContract {
+    id: number
+    customer_id: number
+    customer: IAccount
+    car_id: number
+    car: ICar
+    rent_price: number
+    start_date: Date
+    end_date: Date
+    status: string
+    reason: string
+    insurance_amount: number
+    collateral_type: string
+    is_return_collateral_asset: boolean
+    url: string
+    created_at: Date
+    updated_at: Date
+    receiving_car_images: any[]
+    collateral_asset_images: any[]
+}
