197b762ac9cedef8ef8bf3d9a9f5a91f7a1365d7
eung-eung <boyvip5231@gmail.com>
2024-07-03 00:39:44 +0700

    create payment table component

--- src/app/(views)/contracts/payments/[contractIdSlug]/components/PaymentTable.tsx
+++ src/app/(views)/contracts/payments/[contractIdSlug]/components/PaymentTable.tsx
@@ -1,25 +1,29 @@
 import { IPayment } from '@/app/models/Payment.model'
-import { Button, Table, TableProps } from 'antd'
+import { formatCurrency } from '@/app/utils/formatCurrency'
+import { Button, Table, TableProps, Tag } from 'antd'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 export default function PaymentTable(
     {
-        paymentList
+        listPayment
     }: {
-        paymentList: IPayment[]
+        listPayment?: IPayment[]
     }
 ) {
-
+    const { t } = useTranslation()
     const column: TableProps<IPayment>['columns'] = [
         {
             title: "Loại thanh toán",
             dataIndex: "payment_type",
-            key: "id"
+            key: "id",
+            render: (payment) => t(`common:${payment}`)
         },
         {
             title: "Số tiền",
             dataIndex: "amount",
-            key: "id"
+            key: "id",
+            render: (amount) => formatCurrency(amount)
         },
         {
             title: "Ghi chú",
@@ -29,23 +33,41 @@ export default function PaymentTable(
         {
             title: "Trạng thái",
             dataIndex: "status",
-            key: "id"
+            key: "id",
+            render: (status) => t(`common:${status}`)
         },
         {
             title: "Người thực hiện",
             dataIndex: "payer",
-            key: "id"
+            key: "id",
+            render: (payer) => t(`common:${payer}`)
         },
         {
             title: "",
             key: "id",
-            render: (_, record) => <Button>Thanh toán</Button>
+            render: (_, record) => record.status === 'paid' ?
+                <Tag color='green'>Đã thanh toán</Tag>
+                : <Button>Thanh toán</Button>
         },
     ]
+
+    const rowSelection = {
+        onChange: (selectedRowKeys: React.Key[], selectedRows: IPayment[]) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        },
+        getCheckboxProps: (record: IPayment) => ({
+            disabled: record.payer === 'customer',
+            //   name: record.name,
+        }),
+    };
     return (
         <Table
+            rowKey={(record) => record.id}
+            rowSelection={{
+                ...rowSelection,
+            }}
             columns={column}
-            dataSource={paymentList}
+            dataSource={listPayment}
         />
     )
 }
