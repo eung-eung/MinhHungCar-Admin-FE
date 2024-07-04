d97fda1af7973650ac71eb4472b161ed7582e582
eung-eung <boyvip5231@gmail.com>
2024-07-04 17:24:28 +0700

    update condition

--- src/app/components/TopFilterTable.tsx
+++ src/app/components/TopFilterTable.tsx
@@ -24,7 +24,7 @@ export default function TopFilterTable(
         setRefresh,
         searchValue
     }: {
-        handleChange: any,
+        handleChange?: any,
         handleSearch?: any,
         optionList: Option[],
         defaultValue: any,
