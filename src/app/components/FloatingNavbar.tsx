90693143bce62407a7f5316c085c76656e3690a8
eung-eung <boyvip5231@gmail.com>
2024-07-06 17:11:18 +0700

    update key

--- src/app/components/FloatingNavbar.tsx
+++ src/app/components/FloatingNavbar.tsx
@@ -77,6 +77,7 @@ export const FloatingNav = ({
             >
                 {navItems.map((navItem: any, idx: number) => (
                     <div
+                        key={idx}
                         className={cn(
                             "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
                         )}
