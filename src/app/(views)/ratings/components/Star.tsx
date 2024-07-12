5b8f79a635e539c13442449f73b7fe901c623d16
eung-eung <boyvip5231@gmail.com>
2024-07-12 15:43:43 +0700

    update key

--- src/app/(views)/ratings/components/Star.tsx
+++ src/app/(views)/ratings/components/Star.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
 export default function Star({ rating }: any) {
-    const stars = Array.from({ length: 5 }, () =>
+    const stars = Array.from({ length: 5 }, ({ index }) =>
         <StarBorderRoundedIcon
+            key={index}
             color='disabled'
             fontSize='small' />);
 
