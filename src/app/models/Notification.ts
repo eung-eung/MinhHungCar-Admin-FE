43b8a9d3ba05d0418448db66590f14804d269fa3
eung-eung <boyvip5231@gmail.com>
2024-07-13 18:12:02 +0700

    create notification model

---
+++ src/app/models/Notification.ts
@@ -0,0 +1,11 @@
+export interface INotifcation {
+    id: any,
+    account_id: any,
+    account: any,
+    title: string,
+    content: string,
+    url: string,
+    status: string,
+    created_at: Date,
+    updated_at: Date
+}
