53d221871fee0e77ada65187fe69c491fcb35664
eung-eung <boyvip5231@gmail.com>
2024-07-14 17:53:32 +0700

    handle loading

--- src/app/(views)/cars/[...id]/page.tsx
+++ src/app/(views)/cars/[...id]/page.tsx
@@ -6,25 +6,29 @@ import React, { useEffect, useState } from 'react'
 import CarouselImages from '../components/Carousel';
 import classes from './index.module.css'
 import CarInformation from '../components/CarInformation';
+import { Spin } from 'antd';
 export default function CarDetail({
     params: { id }
 }: {
     params: { id: any, }
 }) {
-    const router = useRouter()
+    const [loading, setLoading] = useState<boolean>(true)
     const axiosAuth = useAxiosAuth()
     const [notFound, setNotFound] = useState<boolean>(true)
     const [detail, setDetail] = useState<ICar>()
     const [refresh, setRefresh] = useState<boolean>(false)
     const getCarDetail = async () => {
         try {
+            setLoading(true)
             const response = await axiosAuth.get('/car/' + id[0])
             console.log(response.data.data);
             setNotFound(false)
             setDetail(response.data.data)
+            setLoading(false)
         } catch (error) {
             console.log('error:', error);
             setNotFound(true)
+            setLoading(false)
         }
 
     }
@@ -34,11 +38,16 @@ export default function CarDetail({
             getCarDetail()
         } else {
             setNotFound(true)
+            setLoading(false)
         }
     }, [id, refresh])
     return (
-        <div>
-            {notFound && <div>404</div>}
+        <div className='relative'>
+            {loading && <div
+                className='flex items-center justify-center' style={{ height: '80vh' }}>
+                <Spin /></div>
+            }
+            {notFound && !loading && <div>404</div>}
             {
                 !notFound && <div className={classes.detail}>
                     <div className={classes.leftDetail}>
