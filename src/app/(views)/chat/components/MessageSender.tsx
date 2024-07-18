0c7f9fb0e52ca144defa4431b9f9d86296467b3b
eung-eung <boyvip5231@gmail.com>
2024-07-18 20:33:57 +0700

    update color chat bubble

--- src/app/(views)/chat/components/MessageSender.tsx
+++ src/app/(views)/chat/components/MessageSender.tsx
@@ -6,6 +6,8 @@ export default function MessageSender(
     return (
         <div className="chat chat-end">
             <div className="chat-bubble" style={{
+                background: '#6C69FF',
+                color: '#fff'
             }}> {content}</div>
         </div>
     )
--- src/app/layouts/components/NotificationList.tsx
+++ src/app/layouts/components/NotificationList.tsx
@@ -33,44 +33,47 @@ export default function NotificationList() {
                 if (data.msg_type === 'ERROR' && session?.access_token) {
                     signOut()
                 } else {
-                    setNotifications((prev: INotifcation[]) => [
-                        {
-                            url: `/chat/${data.conversation_id}`,
-                            title: 'Bạn có tin nhắn mới',
-                        },
-                        ...prev])
+                    if (data.sender !== 'admin') {
+                        setNotifications((prev: INotifcation[]) => [
+                            {
+                                url: `/chat/${data.conversation_id}`,
+                                title: 'Bạn có tin nhắn mới',
+                            },
+                            ...prev])
 
-                    toast.custom((t) => (
-                        <div
-                            style={{ width: '350px' }}
-                            className={`${t.visible ? 'animate-enter' : 'animate-leave'
-                                } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-                        >
-                            <div className="flex-1 w-0 p-4">
-                                <div className="flex items-start">
-                                    <div className="ml-3 flex-1">
-                                        <p className="text-sm font-medium text-gray-900">
-                                            Bạn có tin nhắn mới
-                                        </p>
-                                        {/* <p className="mt-1 text-sm text-gray-500">
-                                    
-                                </p> */}
+                        toast.custom((t) => (
+                            <div
+                                style={{ width: '350px' }}
+                                className={`${t.visible ? 'animate-enter' : 'animate-leave'
+                                    } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+                            >
+                                <div className="flex-1 w-0 p-4">
+                                    <div className="flex items-start">
+                                        <div className="ml-3 flex-1">
+                                            <p className="text-sm font-medium text-gray-900">
+                                                Bạn có tin nhắn mới
+                                            </p>
+                                            {/* <p className="mt-1 text-sm text-gray-500">
+                                        
+                                    </p> */}
+                                        </div>
                                     </div>
                                 </div>
+                                <div className="flex border-l border-gray-200">
+                                    <button
+                                        onClick={() => toast.dismiss(t.id)}
+                                        className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                    >
+                                        Close
+                                    </button>
+                                </div>
                             </div>
-                            <div className="flex border-l border-gray-200">
-                                <button
-                                    onClick={() => toast.dismiss(t.id)}
-                                    className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                                >
-                                    Close
-                                </button>
-                            </div>
-                        </div>
-                    ), {
-                        position: "bottom-left"
-                    })
-                    soundAudioRef.current?.play()
+                        ), {
+                            position: "bottom-left"
+                        })
+                        soundAudioRef.current?.play()
+                    }
+
                 }
 
             }
